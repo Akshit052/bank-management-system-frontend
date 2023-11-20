7b7cb79324cd292ff30e1b3828fde3f01c628127
Akshit Garg <akshit.garg@publicissapient.com>
2023-11-20 22:51:24 +0530

    Frontend done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import LoginSignup from './Components/LoginSignup/LoginSignup';
+// App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import LoginComponent from './components/LoginComponent';
+import DashboardComponent from './components/DashboardComponent';
+import DepositComponent from './components/DepositComponent';
+import WithdrawComponent from './components/WithdrawComponent';
+import TransferComponent from './components/TransferComponent';
+import ViewBalanceComponent from './components/ViewBalanceComponent';
+import ViewStatementComponent from './components/ViewStatementComponent';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
-    <div>
-      <LoginSignup/>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" component={LoginComponent} />
+        <Route path="/dashboard" component={DashboardComponent} />
+        <Route path="/deposit" component={DepositComponent} />
+        <Route path="/withdraw" component={WithdrawComponent} />
+        <Route path="/transfer" component={TransferComponent} />
+        <Route path="/view-balance" component={ViewBalanceComponent} />
+        <Route path="/view-statement" component={ViewStatementComponent} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
